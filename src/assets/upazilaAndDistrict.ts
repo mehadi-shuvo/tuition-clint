b83a68e8b2007698b6516b78d6ee0689c8dd36f7
mehadi-shuvo <mehadihasanshuvo88@gmail.com>
2024-05-11 11:00:25 +0600

    student profile update

---
+++ src/assets/upazilaAndDistrict.ts
@@ -0,0 +1,98 @@
+export type TUpazila = {
+  district: string;
+  thanas: string[];
+};
+
+export const arrayOfUpazilaAndDistrict: TUpazila[] = [
+  {
+    district: "Dhaka",
+    thanas: [
+      "Adabar",
+      "Badda",
+      "Bangsal",
+      "Bimanbandar",
+      "Cantonment",
+      "Chak Bazar",
+      "Darus Salam",
+      "Demra",
+      "Dhamrai",
+      "Dhanmondi",
+      "Gendaria",
+      "Gulshan",
+      "Hazaribagh",
+      "Jatrabari",
+      "Kadamtali",
+      "Kafrul",
+      "Kalabagan",
+      "Kamrangirchar",
+      "Khilgaon",
+      "Khilkhet",
+      "Kotwali",
+      "Lalbagh",
+      "Mirpur",
+      "Mohammadpur",
+      "Motijheel",
+      "New Market",
+      "Pallabi",
+      "Paltan",
+      "Ramna",
+      "Sabujbagh",
+      "Shah Ali",
+      "Shahbagh",
+      "Sher-e-Bangla Nagar",
+      "Shyampur",
+      "Sutrapur",
+      "Tejgaon",
+      "Turag",
+      "Uttar Khan",
+      "Uttara",
+      "Wari",
+    ],
+  },
+  {
+    district: "Gazipur",
+    thanas: ["Gazipur Sadar", "Kaliakair", "Kaliganj", "Kapasia", "Sreepur"],
+  },
+  {
+    district: "Savar",
+    thanas: ["Savar", "Ashulia", "Dhamsona", "Karnapara", "Bank Town"],
+  },
+  {
+    district: "Narayanganj",
+    thanas: [
+      "Araihazar",
+      "Bandar",
+      "Narayanganj Sadar",
+      "Rupganj",
+      "Siddhirganj",
+    ],
+  },
+  {
+    district: "Rajshahi",
+    thanas: ["Boalia", "Charghat", "Durgapur", "Godagari", "Paba"],
+  },
+  {
+    district: "Rangpur",
+    thanas: ["Badarganj", "Gangachara", "Kaunia", "Rangpur Sadar", "Taraganj"],
+  },
+  {
+    district: "Sylhet",
+    thanas: [
+      "Beanibazar",
+      "Bishwanath",
+      "Golapganj",
+      "Gowainghat",
+      "Sylhet Sadar",
+    ],
+  },
+  {
+    district: "Sirajganj",
+    thanas: [
+      "Belkuchi",
+      "Kamarkhanda",
+      "Kazipur",
+      "Raiganj",
+      "Sirajganj Sadar",
+    ],
+  },
+];
--- src/pages/Student/StudentProfile.tsx
+++ src/pages/Student/StudentProfile.tsx
@@ -11,7 +11,7 @@ import "../../App.css";
 import { SubmitHandler, useForm } from "react-hook-form";
 import {
   useCreatePostMutation,
-  useGetThanasQuery,
+  // useGetThanasQuery,
 } from "../../redux/features/post/postApi";
 import { useGetOneStudentByIdQuery } from "../../redux/features/student/studentApi";
 import { useAppSelector } from "../../redux/hooks";
@@ -22,12 +22,16 @@ import toast from "react-hot-toast";
 import StudentTuitions from "./StudentTuitions";
 import { allDistrictsArray } from "../../assets/district";
 import { useState } from "react";
+import {
+  TUpazila,
+  arrayOfUpazilaAndDistrict,
+} from "../../assets/upazilaAndDistrict";
 
 const StudentProfile = () => {
   const user: TUser | null = useAppSelector(useAuthCurrentUser);
-  const [params, setParams] = useState<TParamsQuery[] | undefined>(undefined);
-
-  const { data: thanas } = useGetThanasQuery(params);
+  // const [params, setParams] = useState<TParamsQuery[] | undefined>(undefined);
+  const [upazilas, setUpazilas] = useState<TUpazila | undefined>(undefined);
+  // const { data: thanas } = useGetThanasQuery(params);
   const { isLoading, data: student } = useGetOneStudentByIdQuery(user?.email);
   if (isLoading) {
     return (
@@ -40,9 +44,15 @@ const StudentProfile = () => {
   const [createPost] = useCreatePostMutation();
 
   const handleDistrict = (value: string) => {
-    const queryParams: TParamsQuery[] = [];
-    queryParams.push({ name: "district", value: value });
-    setParams(queryParams);
+    // const queryParams: TParamsQuery[] = [];
+    // queryParams.push({ name: "district", value: value });
+    // setParams(queryParams);
+
+    const matchedDistrict = arrayOfUpazilaAndDistrict.find(
+      (dis: TUpazila) => dis.district.toLowerCase() === value
+    );
+
+    setUpazilas(matchedDistrict);
   };
 
   const { register, handleSubmit, reset } = useForm<TPost>();
@@ -142,7 +152,7 @@ const StudentProfile = () => {
                   <option disabled selected>
                     select Upazila
                   </option>
-                  {thanas?.data?.thanas.map((tha: string) => (
+                  {upazilas?.thanas.map((tha: string) => (
                     <option key={tha} value={tha.toLowerCase()}>
                       {tha}
                     </option>
