78f9d276e61c0992cad5d75002541f4cbf245577
mehadi-shuvo <mehadihasanshuvo88@gmail.com>
2025-08-03 22:28:39 +0600

    update: Title hooks

--- src/utils/useTitle.ts
+++ src/utils/useTitle.ts
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 
 const useTitle = (title: string) => {
   useEffect(() => {
-    document.title = "Tuition Point: " + title;
+    document.title = title + "| Teacher Lagbe";
   }, [title]);
 };
 
